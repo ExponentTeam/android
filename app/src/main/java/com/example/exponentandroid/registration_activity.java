836a5dcb12408e8cd85384ef932d02330148e40d
Mandros <maks10031003@gmail.com>
2021-01-20 20:11:59 +0200

    Added missing files

--- app/src/main/java/com/example/exponentandroid/registration_activity.java
+++ app/src/main/java/com/example/exponentandroid/registration_activity.java
@@ -5,12 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class registration_activity extends AppCompatActivity {
-    EditText email_edittext, password_edittext;
+    EditText email2_edittext, password2_edittext, reenter_password2_edittext;
+    private FirebaseAuth Auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registration_activity);
     }
+
+    private void init(){}
 }
