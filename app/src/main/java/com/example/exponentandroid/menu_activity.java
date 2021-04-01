0883f039ddc6d22b2d1680eccc0bb901f70dd00e
Meksich <maks10031003@gmail.com>
2021-04-01 15:32:39 +0300

    Visual improvements, Added swipe ability in main menu

--- app/src/main/java/com/example/exponentandroid/menu_activity.java
+++ app/src/main/java/com/example/exponentandroid/menu_activity.java
@@ -3,20 +3,45 @@ package com.example.exponentandroid;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.example.exponentandroid.fragments.PageFragmentAboutUs;
+import com.example.exponentandroid.fragments.PageFragmentCameraListening;
+import com.example.exponentandroid.fragments.PageFragmentCarInfo;
+import com.example.exponentandroid.fragments.PageFragmentJPS;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
-public class menu_activity extends Activity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class menu_activity extends AppCompatActivity {
+
+    private ViewPager pager;
+    private PagerAdapter pagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_activity);
+
+        List<Fragment> list = new ArrayList<>();
+        list.add(new PageFragmentCameraListening());
+        list.add(new PageFragmentJPS());
+        list.add(new PageFragmentCarInfo());
+        list.add(new PageFragmentAboutUs());
+
+        pager = findViewById(R.id.pager);
+        pagerAdapter = new PagerSwipeApdater(getSupportFragmentManager(), list);
+
+        pager.setAdapter(pagerAdapter);
+        pager.setCurrentItem(2);
     }
 
 }
