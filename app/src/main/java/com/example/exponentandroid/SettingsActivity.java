0e057ccb2ea45f33fa32ed2820ea041e57b86aaf
Voooovka <vovkavysochanskyi@gmail.com>
2021-10-15 15:28:32 +0300

    Adding some fields to settings page and some strings to string.xml

--- app/src/main/java/com/example/exponentandroid/SettingsActivity.java
+++ app/src/main/java/com/example/exponentandroid/SettingsActivity.java
@@ -3,12 +3,41 @@ package com.example.exponentandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    String[] data = {"Ukr", "Eng", "Pol", "Grm", "Jpn"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, data);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        spinner.setAdapter(adapter);
+
+        spinner.setPrompt("Title");
+
+        spinner.setSelection(1);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+
+                Toast.makeText(getBaseContext(), "Position = " + position, Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+            }
+        });
     }
 }
