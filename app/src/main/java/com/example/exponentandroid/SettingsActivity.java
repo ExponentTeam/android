385ba65e13d2f5c09631cb72559ed3a133a7da2b
Voooovka <vovkavysochanskyi@gmail.com>
2021-10-22 20:39:55 +0300

    adding localization(change of language)

--- app/src/main/java/com/example/exponentandroid/SettingsActivity.java
+++ app/src/main/java/com/example/exponentandroid/SettingsActivity.java
@@ -30,28 +30,6 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, data);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
-        spinner.setAdapter(adapter);
-
-        spinner.setPrompt("Title");
-
-        spinner.setSelection(1);
-
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view,
-                                       int position, long id) {
-
-                Toast.makeText(getBaseContext(), "Position = " + position, Toast.LENGTH_SHORT).show();
-            }
-            @Override
-            public void onNothingSelected(AdapterView<?> arg0) {
-            }
-        });
-
         userEmail = findViewById(R.id.tvUserEmail);
         userLogout = findViewById(R.id.btnLogout);
 
