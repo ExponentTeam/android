16b592bd29cdc05a060ed5ab0c70b7e3efe9fda5
Voooovka <vovkavysochanskyi@gmail.com>
2021-12-06 21:13:14 +0200

    adding camera broadcasting, adding button to get notification and some minore changes in other files

--- app/src/main/java/com/example/exponentandroid/SettingsActivity.java
+++ app/src/main/java/com/example/exponentandroid/SettingsActivity.java
@@ -1,8 +1,16 @@
 package com.example.exponentandroid;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static androidx.core.app.NotificationCompat.PRIORITY_HIGH;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -15,6 +23,8 @@ import android.widget.Toast;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.concurrent.TimeUnit;
+
 public class SettingsActivity extends AppCompatActivity {
 
     TextView userEmail;
@@ -23,7 +33,11 @@ public class SettingsActivity extends AppCompatActivity {
     FirebaseAuth firebaseAuth;
     FirebaseUser firebaseUser;
 
-    String[] data = {"Ukr", "Eng", "Pol", "Grm", "Jpn"};
+    private NotificationManager notificationManager;
+    private static final int NOTIFY_ID = 1;
+
+    private static final String CHANNEL_ID = "CHANNEL_ID";
+    Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,5 +62,41 @@ public class SettingsActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        setContentView(R.layout.settings_activity);
+        b1 = findViewById(R.id.button);
+        notificationManager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+                NotificationCompat.Builder notificationBuilder =
+                        new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID)
+                                .setAutoCancel(false)
+                                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                                .setWhen(System.currentTimeMillis())
+                                .setContentIntent(pendingIntent)
+                                .setContentTitle("ALARM")
+                                .setContentText("SOMEONE IS TRYING TO STEAL YOUR CAR")
+                                .setPriority(PRIORITY_HIGH);
+                createChannelIfNeeded(notificationManager);
+                try {
+                    TimeUnit.SECONDS.sleep(5);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                notificationManager.notify(NOTIFY_ID, notificationBuilder.build());
+            }
+        });
+    }
+    public static void createChannelIfNeeded(NotificationManager manager) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, CHANNEL_ID, NotificationManager.IMPORTANCE_DEFAULT);
+            manager.createNotificationChannel(notificationChannel);
+        }
     }
 }
--- app/src/main/java/com/example/exponentandroid/fragments/PageFragmentCameraListening.java
+++ app/src/main/java/com/example/exponentandroid/fragments/PageFragmentCameraListening.java
@@ -1,12 +1,16 @@
 package com.example.exponentandroid.fragments;
 
+import android.annotation.TargetApi;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -27,9 +31,35 @@ public class PageFragmentCameraListening extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         ViewGroup inflate = (ViewGroup) inflater.inflate(R.layout.cameras_listening, container, false);
 
+        WebView webView = inflate.findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("http://1626-46-211-98-151.ngrok.io/mjpeg/1");
 
+        WebViewClient webViewClient = new WebViewClient() {
 
+            @SuppressWarnings("deprecation") @Override
 
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+                view.loadUrl(url);
+
+                return true;
+
+            }
+
+            @TargetApi(Build.VERSION_CODES.N) @Override
+
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+
+                view.loadUrl(request.getUrl().toString());
+
+                return true;
+
+            }
+
+        };
+
+        webView.setWebViewClient(webViewClient);
 
 
         return inflate;
